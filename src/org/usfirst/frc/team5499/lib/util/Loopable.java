035badafb4b3be0a796abd69e397e7b38b0ac92e
Connor Worley <connorbworley@gmail.com>
2016-03-21 15:29:24 -0700

    clear out old src

--- src/org/usfirst/frc/team5499/lib/util/Loopable.java
+++
@@ -1,5 +0,0 @@
-package org.usfirst.frc.team5499.lib.util;
-
-public interface Loopable {
-	public void update();
-}
--- src/org/usfirst/frc/team5499/robot/controllers/PIDInput.java
+++
@@ -1,5 +0,0 @@
-package org.usfirst.frc.team5499.robot.controllers;
-
-public interface PIDInput {
-	public double getInput();
-}
