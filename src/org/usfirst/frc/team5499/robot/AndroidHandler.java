25fbc2cfeb6fa2c48ffce1b51e84c3900ad45f5f
James Bartlett <jmabartlett2@gmail.com>
2016-04-27 11:24:26 -0700

    added http request to androidhandler

--- src/org/usfirst/frc/team5499/robot/AndroidHandler.java
+++ src/org/usfirst/frc/team5499/robot/AndroidHandler.java
@@ -1,9 +1,16 @@
 package org.usfirst.frc.team5499.robot;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 import org.json.JSONObject;
 import org.spectrum3847.RIOdroid.RIOadb;
 import org.spectrum3847.RIOdroid.RIOdroid;
 import org.usfirst.frc.team5499.lib.util.Loopable;
+
+import java.net.URLConnection;
 
 public class AndroidHandler implements Loopable {
 
@@ -17,8 +24,8 @@ public class AndroidHandler implements Loopable {
 	public void init(){
 		RIOdroid.init();
     	RIOadb.init();
-    	RIOdroid.executeCommand("adb start-server");
-    	RIOdroid.executeCommand("adb forward tcp:80 tcp:8080");
+    	System.out.println(RIOdroid.executeCommand("adb start-server"));
+    	System.out.println(RIOdroid.executeCommand("adb forward tcp:80 tcp:8080"));
 	}
 	
 	@Override
@@ -32,5 +39,24 @@ public class AndroidHandler implements Loopable {
 		dist = json.getDouble("dist");
 		
 	}
-
+	
+	public String get(){
+		String ret = "";
+		URLConnection c;
+		BufferedReader in;
+		try{
+		
+		URL url = new URL("localhost:80");
+		c = url.openConnection();
+		in = new BufferedReader(new InputStreamReader(c.getInputStream()));
+		
+		ret = in.readLine();
+		in.close();
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		
+		return ret;
+		
+	}
 }
